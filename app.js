7cb1e85c311a2ef5058200d87d4e7b56418717c5
Jason Liu <jaliu@tcd.ie>
2023-09-11 22:51:08 +0100

    test again

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos || currentScrollPos > 100) {
+  if (prevScrollpos > currentScrollPos || currentScrollPos < 100) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-160px";
